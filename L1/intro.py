390ffe54db83fb80f043478ad9b5766f8fcde25e
SaminFlicy <ibrahimjayed2312@gmail.com>
2026-09-19 22:42:56 +0600

    CW or HW

--- L1/intro.py
+++ L1/intro.py
@@ -1,1 +1,7 @@
-print("Hello Guys")
+
+name = input("What is your name? ")
+grade = input("What is your grade? ")
+curriculum = input("What curriculum are you following? ")
+city = input("What city are you from? ")
+
+print(f"Hello {name}! You are in grade {grade} following the {curriculum} curriculum and you are from {city}.")
